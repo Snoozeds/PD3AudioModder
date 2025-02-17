e9fbd3dffa8c23b365c60612c5db93b463bd08ef
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2025-02-17 12:58:37 +0000

    Fix licenses window crashes (revert changes)

--- src/views/LicensesWindow.axaml.cs
+++ src/views/LicensesWindow.axaml.cs
@@ -1,39 +1,20 @@
 ﻿using Avalonia.Controls;
-using System;
+using Avalonia.Interactivity;
 using System.Diagnostics;
-using System.Windows.Input;
-using ReactiveUI;
-using System.Reactive;
-using System.Reactive.Linq;
+using System;
 
 namespace PD3AudioModder
 {
     public partial class LicensesWindow : Window
     {
-        public ICommand WwisePd3Command { get; }
-        public ICommand TwemojiCommand { get; }
-        public ICommand CloseCommand { get; }
-
         public LicensesWindow()
         {
             InitializeComponent();
-            WwisePd3Command = ReactiveCommand.Create(
-                () => OpenUrl("https://github.com/MoolahModding/wwise_pd3"),
-                outputScheduler: RxApp.MainThreadScheduler);
-
-            TwemojiCommand = ReactiveCommand.Create(
-                () => OpenUrl("https://github.com/twitter/twemoji"),
-                outputScheduler: RxApp.MainThreadScheduler);
-
-            CloseCommand = ReactiveCommand.Create(
-                () => this.Close(),
-                outputScheduler: RxApp.MainThreadScheduler);
-
-            DataContext = this;
         }
 
-        private void OpenUrl(string url)
+        private void WwisePD3Click(object sender, RoutedEventArgs e)
         {
+            string url = "https://github.com/MoolahModding/wwise_pd3";
             try
             {
                 Process.Start(new ProcessStartInfo
@@ -45,6 +26,26 @@ namespace PD3AudioModder
             catch (Exception)
             {
             }
+        }
+
+        private void TwemojiClick(object sender, RoutedEventArgs e) {
+            string url = "https://github.com/twitter/twemoji";
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void CloseClick(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
